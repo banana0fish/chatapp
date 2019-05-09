991eb7459b585eb7e21072515c711a391f11512a
Donnie Waite <donnie.e.waite@gmail.com>
2019-05-09 10:42:26 -0400

    fixed auth problems and created panels for home screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react'
+import { connect } from 'react-redux'
 import './App.css';
 
 
@@ -8,10 +9,10 @@ import SidePanel from './SidePanel/SidePanel'
 import Messages from './Messages/Messages'
 import MetaPanel from './MetaPanel/MetaPanel'
 
-const App = () => (
+const App = ({ currentUser }) => (
   <Grid columns="equal" className="app" style={{ background: '#eee'}}>
     <ColorPanel />
-    <SidePanel />
+    <SidePanel currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320}}>
       <Messages />
@@ -24,4 +25,8 @@ const App = () => (
   </Grid>
 )
 
-export default App;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(App);
--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import firebase from '../../firebase'
 
 class UserPanel extends React.Component {
+  state = {
+    user: this.props.currentUser
+  }
+
 
 dropdownOptions = () => [
   {
     key: "user",
-    text: <span>Signed In as <strong>User</strong></span>,
+    text: <span>Signed In as <strong>{this.state.user.displayName}</strong></span>,
     disabled: true
   },
   {
@@ -15,11 +21,19 @@ dropdownOptions = () => [
   },
   {
     key: "signout",
-    text: <span>Sign Out</span>
+    text: <span onClick={this.handleSignout}>Sign Out</span>
   }
 ]
 
+handleSignout = () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => console.log('signed out!'))
+}
+
   render() {
+
     return(
       <Grid style={{ background: '#4c3c4c'}}>
         <Grid.Column>
@@ -32,7 +46,7 @@ dropdownOptions = () => [
           {/* user dropdown */}
           <Header style={{ padding: '0.25em'}} as="h4" inverted>
             <Dropdown trigger={
-              <span>User</span>}
+              <span>{this.state.user.displayName}</span>}
               options={ this.dropdownOptions()}/>
           </Header>
         </Grid.Column>
@@ -42,4 +56,8 @@ dropdownOptions = () => [
   }
 }
 
-export default UserPanel
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(UserPanel)
